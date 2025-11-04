51acf7c687c1f2cdbdcfd121677a26931fc43241
Shiny Lee <shinish512@gmail.com>
2025-11-04 22:37:24 +0900

    alarm list 컴포넌트 생성

---
+++ lib/features/alarm/component/alarm_row_like.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AlarmRowLike extends StatelessWidget {
+  const AlarmRowLike({super.key, alarmId, userId, postId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Color(0xffd9d9d9), width: 0.3)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          spacing: 15,
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundColor: Color(0xffe8e8dc),
+              child: Icon(
+                Icons.favorite_border_rounded,
+                size: 20,
+                color: Color(0xff5b89b2),
+              ),
+            ),
+            Column(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  child: Text(
+                    "userId.nickname님이 회원님의 게시물을 좋아합니다",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13,
+                      color: Color(0xff515151),
+                    ),
+                  ),
+                ),
+                Text(
+                  "n시간 전",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    fontSize: 10,
+                    color: Color(0xff515151),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
