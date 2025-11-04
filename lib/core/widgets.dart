5290662c8529979d446f8d0616802cd6c41c12b5
yang-yohan <zanta2001@naver.com>
2025-11-04 15:57:20 +0900

    첫번째 작업

---
+++ lib/core/widgets.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class OpicAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const OpicAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Image.asset('assets/images/logo_long_skyblue.png'),
+          ),
+          Row(
+            children: [
+              Container(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications_none),
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.exit_to_app),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(50);
+}
+
+class OpicBottomNav extends StatelessWidget {
+  const OpicBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      selectedIconTheme: IconThemeData(color: Color(0xFF95B7DB)),
+      unselectedIconTheme: IconThemeData(color: Colors.grey),
+      selectedItemColor: Color(0xFF95B7DB),
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: '홈'),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: '친구'),
+        BottomNavigationBarItem(icon: Icon(Icons.image), label: '내 피드'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+      ],
+    );
+  }
+}
