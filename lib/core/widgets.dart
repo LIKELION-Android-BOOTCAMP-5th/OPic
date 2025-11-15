a25231d59d5f0b1fcf2c6ba43c502ea944bbb7dc
Shiny Lee <shinish512@gmail.com>
2025-11-16 00:32:42 +0900

    알람 빨간점 수정

--- lib/core/widgets.dart
+++ lib/core/widgets.dart
@@ -3,18 +3,39 @@ import 'package:go_router/go_router.dart';
 import 'package:opicproject/core/app_colors.dart';
 import 'package:opicproject/core/manager/autn_manager.dart';
 import 'package:opicproject/features/alarm/data/alarm_view_model.dart';
-import 'package:opicproject/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:provider/provider.dart';
 
-class OpicAppbar extends StatelessWidget implements PreferredSizeWidget {
+class OpicAppbar extends StatefulWidget implements PreferredSizeWidget {
   const OpicAppbar({super.key});
 
   @override
+  State<OpicAppbar> createState() => _OpicAppbarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+}
+
+class _OpicAppbarState extends State<OpicAppbar> {
+  @override
+  void initState() {
+    super.initState();
+    // 앱바가 생성될 때 알람 로드
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final authManager = context.read<AuthManager>();
+      if (authManager.userInfo != null) {
+        context.read<AlarmViewModel>().fetchAlarms(
+          1,
+          authManager.userInfo?.id ?? 0,
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<AuthManager>(
-      builder: (context, authManager, child) {
+    return Consumer2<AuthManager, AlarmViewModel>(
+      builder: (context, authManager, alarmViewModel, child) {
         final isLoggedIn = authManager.userInfo != null;
-        final authViewModel = context.read<AuthViewModel>();
 
         return SizedBox(
           height: MediaQuery.of(context).size.height * 0.05,
@@ -28,35 +49,31 @@ class OpicAppbar extends StatelessWidget implements PreferredSizeWidget {
               Row(
                 children: [
                   if (isLoggedIn)
-                    Consumer<AlarmViewModel>(
-                      builder: (context, alarmViewModel, child) {
-                        return Stack(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                context.push('/alarm_list_page');
-                              },
-                              icon: Icon(
-                                Icons.notifications_none,
-                                color: AppColors.opicSoftBlue,
+                    Stack(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            context.push('/alarm_list_page');
+                          },
+                          icon: Icon(
+                            Icons.notifications_none,
+                            color: AppColors.opicSoftBlue,
+                          ),
+                        ),
+                        if (alarmViewModel.hasUnreadAlarm)
+                          Positioned(
+                            right: 8,
+                            top: 8,
+                            child: Container(
+                              width: 6,
+                              height: 6,
+                              decoration: BoxDecoration(
+                                color: AppColors.opicRed,
+                                shape: BoxShape.circle,
                               ),
                             ),
-                            if (alarmViewModel.hasUnreadAlarm)
-                              Positioned(
-                                right: 8,
-                                top: 8,
-                                child: Container(
-                                  width: 6,
-                                  height: 6,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.opicRed,
-                                    shape: BoxShape.circle,
-                                  ),
-                                ),
-                              ),
-                          ],
-                        );
-                      },
+                          ),
+                      ],
                     ),
                   isLoggedIn
                       ? Container(
@@ -89,9 +106,6 @@ class OpicAppbar extends StatelessWidget implements PreferredSizeWidget {
       },
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(50);
 }
 
 /// 하단네비게이션
